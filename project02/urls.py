02cc986197b3ec8666a98ea01d485248e293b658
danastyles <dana.oshakbayeva@nu.edu.kz>
2020-04-16 02:47:03 +0600

    first commit

--- project02/urls.py
+++ project02/urls.py
@@ -33,7 +33,7 @@ if settings.DEBUG:
     _media_url = settings.MEDIA_URL
     if _media_url.startswith('/'):
         _media_url = _media_url[1:]
-        urlpatterns += patterns('',
+        urlpatterns += urlpatterns('',
             r'^%s(?P<path>.*)$' % _media_url, serve, 
-            {'document_root': settings.MEDIA_ROOT}))
+            {'document_root': settings.MEDIA_ROOT})
     del(_media_url, serve)
